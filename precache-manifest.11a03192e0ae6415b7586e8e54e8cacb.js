65dcf2a93c91100ea49e588eddb1493484b73afb
Matheus de Moraes <theusmoraes@gmail.com>
2020-05-05 22:04:35 -0300

    Updates

---
+++ precache-manifest.11a03192e0ae6415b7586e8e54e8cacb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfa69d3b48a40af091d12f3fa2bd762",
+    "url": "/Github-Profile/index.html"
+  },
+  {
+    "revision": "b4a77c545dd573a2fce6",
+    "url": "/Github-Profile/static/css/main.ec5c2df7.chunk.css"
+  },
+  {
+    "revision": "f05498f475364af67241",
+    "url": "/Github-Profile/static/js/2.4ef79c60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Github-Profile/static/js/2.4ef79c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a77c545dd573a2fce6",
+    "url": "/Github-Profile/static/js/main.ff11bce5.chunk.js"
+  },
+  {
+    "revision": "4dcb6a1f0211a75e076b",
+    "url": "/Github-Profile/static/js/runtime-main.c8d6e266.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ca5460a998586b752abe06bd02a2db6.js"
+  "/Github-Profile/precache-manifest.11a03192e0ae6415b7586e8e54e8cacb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Github-Profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
