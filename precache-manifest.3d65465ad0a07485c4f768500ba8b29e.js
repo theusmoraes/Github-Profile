778c1993d7645eb00ac08ad487511e67597a191f
Matheus de Moraes <theusmoraes@gmail.com>
2020-05-05 21:15:55 -0300

    Updates

---
+++ precache-manifest.3d65465ad0a07485c4f768500ba8b29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6b435bcb0ae5c8bb5f0cf2b58fcd70",
+    "url": "/github-profile/index.html"
+  },
+  {
+    "revision": "b4a77c545dd573a2fce6",
+    "url": "/github-profile/static/css/main.ec5c2df7.chunk.css"
+  },
+  {
+    "revision": "f05498f475364af67241",
+    "url": "/github-profile/static/js/2.4ef79c60.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-profile/static/js/2.4ef79c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a77c545dd573a2fce6",
+    "url": "/github-profile/static/js/main.ff11bce5.chunk.js"
+  },
+  {
+    "revision": "64b166f500a82fe31260",
+    "url": "/github-profile/static/js/runtime-main.cee50a0b.js"
+  }
+]);
